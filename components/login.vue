ab0686f44597badb6ac7aca74dd6598534a5d5c7
hyeminem-Jo <konnimey@naver.com>
2023-02-03 09:03:26 +0900

    커밋222

--- components/login.vue
+++ components/login.vue
@@ -24,29 +24,26 @@ export default {
       username: '',
       password: '',
       response: {}, // test
-      success: { // test
-        code: "SUC001",
-        message: "처리가 완료되었습니다.",
-        data: {
-          accessToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtZXRhRnJvbnRUb2tlbiBhY2Nlc3NUb2tlbiIsImV4cCI6MTY3NDgxMjg0OSwidG9rZW5JbmZvIjoiUk5BdzRZYXlPcTdxNEtHYXQ2SmNtVGpHS3llbkR2TEpaNko2TFNVd0szZkMreldMWEdINzdETHk5Q3BMRW81dzkwT0lOL2pueTBpVU1BR2NDQXhUVlNVQStML21lSEU5WkwzTThYOC9JblIrZlhqK0RNb1Eyd1Y5d1BuTDlLMEFFT0pNeCtQK0syZjdXdSsyYW9OK0d5cXdlV01OVy9BeEFLSy8xZldXS3UxRjFwdEFFS1ZvYzZmc3JFd3FVdE9WIn0.F5OtWcaGO3VYJOtPqVMVY2gMnDh9mdhXTXwTc8_ib20sq_DBjgIsGk0d5FhtuvVOOxBsb0c-8iFgBlUG71t35w",
-          accessTokenExpiredDt: "2023-01-27 18:47:29.342",
-          memberSeq: 100000014,
-          id: "fdev1",
-          email: "hyoungwook@emotion.co.kr",
-          passwordExpiredYn: false
-        }
-      }, 
-      failure: {
-        "code": "ERR_LOGIN_001",
-        "message": "일치하는 회원 정보가 없는 경우",
-        "data": null
-      }
+      // success: { // test
+      //   code: "SUC001",
+      //   message: "처리가 완료되었습니다.",
+      //   data: {
+      //     accessToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtZXRhRnJvbnRUb2tlbiBhY2Nlc3NUb2tlbiIsImV4cCI6MTY3NDgxMjg0OSwidG9rZW5JbmZvIjoiUk5BdzRZYXlPcTdxNEtHYXQ2SmNtVGpHS3llbkR2TEpaNko2TFNVd0szZkMreldMWEdINzdETHk5Q3BMRW81dzkwT0lOL2pueTBpVU1BR2NDQXhUVlNVQStML21lSEU5WkwzTThYOC9JblIrZlhqK0RNb1Eyd1Y5d1BuTDlLMEFFT0pNeCtQK0syZjdXdSsyYW9OK0d5cXdlV01OVy9BeEFLSy8xZldXS3UxRjFwdEFFS1ZvYzZmc3JFd3FVdE9WIn0.F5OtWcaGO3VYJOtPqVMVY2gMnDh9mdhXTXwTc8_ib20sq_DBjgIsGk0d5FhtuvVOOxBsb0c-8iFgBlUG71t35w",
+      //     accessTokenExpiredDt: "2023-01-27 18:47:29.342",
+      //     memberSeq: 100000014,
+      //     id: "fdev1",
+      //     email: "hyoungwook@emotion.co.kr",
+      //     passwordExpiredYn: false
+      //   }
+      // }, 
+      // failure: {
+      //   "code": "ERR_LOGIN_001",
+      //   "message": "일치하는 회원 정보가 없는 경우",
+      //   "data": null
+      // }
     }
   },
   methods: {
-    errorTest () {
-
-    },
     async submitForm() {
       try {
         // 서버 요청 에러 때문에 잠시 setTimeout 으로 비동기를 구현하여 더미테스트를 진행. (이때 setTimeout 은 Promise 를 반환하지 않기 때문에 async await 를 써도 동기적으로 굴러가지 않아 임의로 Promise 를 반환하게 해주었다.)
